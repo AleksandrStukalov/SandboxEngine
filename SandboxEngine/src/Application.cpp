0cd09501467fdc19a6b902ced8639a463b089ef6
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2022-07-21 12:35:04 +0300

    Added GLAD building with CMake

--- SandboxEngine/src/Application.cpp
+++ SandboxEngine/src/Application.cpp
@@ -2,8 +2,10 @@
 
 #define GLFW_INCLUDE_NONE
 #include "GLFW/glfw3.h"
+#include "glad/glad.h"
 
 GLFWwindow* window;
+unsigned int vb;
 
 SE::Application::Application()
 {
@@ -21,11 +23,31 @@ SE::Application::Application()
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader(GLADloadproc(glfwGetProcAddress)))
+    {
+        std::cout << "Failed to initialize glad" << std::endl;
+    }
+
+
+    float positions[]
+    {
+        -0.5f, -0.5f, 0.0f, // Left
+        0.5f, -0.5f, 0.0f,  // Right
+        0.0f, 0.5f, 0.0f	// Top
+    };
+
+    glGenBuffers(1, &vb);
+    glBindBuffer(GL_ARRAY_BUFFER, vb);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(float) * 3, (void*)0);
+    glEnableVertexAttribArray(0);// Enables OpenGL's default vertex array.
 }
 
 SE::Application::~Application()
 {
-    
+    glDeleteBuffers(1, &vb);
+    glfwTerminate();
 }
 
 void SE::Application::update()
@@ -45,5 +67,8 @@ void SE::Application::update()
 
 void SE::Application::onUpdate()
 {
-    
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
