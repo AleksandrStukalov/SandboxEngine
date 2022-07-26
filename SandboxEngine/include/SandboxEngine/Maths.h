5eeff6192ad1d2a55a7c6d5916399ec3225466da
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2022-07-26 20:47:14 +0300

    Added glm support

--- SandboxEngine/include/SandboxEngine/Maths.h
+++ SandboxEngine/include/SandboxEngine/Maths.h
@@ -99,5 +99,4 @@ namespace SE
 
     };
 
-
 }
--- TestProject/src/main.cpp
+++ TestProject/src/main.cpp
@@ -6,6 +6,8 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
+#include "glm/glm.hpp"
+
 #include <iostream>
 #include <sstream>
 #include <memory>
